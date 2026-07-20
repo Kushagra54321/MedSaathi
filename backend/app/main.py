c71eab06c1be16647f748938ff6dae8289a8ef95
Kushagra Singh <Kushagrasingh54321@gmail.com>
2026-07-20 10:59:42 +0530

    Phase 1: FastAPI setup and file upload completed

---
+++ backend/app/main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI, UploadFile, File , HTTPException
+import shutil
+import os
+app = FastAPI()
+@app.get("/")
+def home():
+    return {"message": "Hello, world"}
+@app.post("/uploadfile/")
+async def upload_file(file: UploadFile = File(...)):
+
+    allowed_types = [
+        "application/pdf",
+        "image/jpeg",
+        "image/png"
+    ]
+
+    if file.content_type not in allowed_types:
+        raise HTTPException(
+            status_code=400,
+            detail="Only PDF, JPG and PNG files are allowed."
+        )
+    
+    upload_folder = "uploads"
+
+    os.makedirs(upload_folder, exist_ok=True)
+
+    file_path = os.path.join(upload_folder, file.filename)
+
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    return {
+        "message": "File uploaded successfully",
+        "filename": file.filename,
+        "saved_path": file_path
+    }
